f29a0dbd3c41ad140e1424ab0fea4083bc00b94a
Tristin Schlauch <nmbr1hockeyfan@gmail.com>
2025-04-10 18:09:31 -0600

    Updated Mad Libs to include checklist

--- Mad Libs.py
+++ Mad Libs.py
@@ -1,14 +1,13 @@
 # Step 1:
-# Use a print statement to print a welcome message to the player.
+# [] Use a print statement to print a welcome message to the player.
 # Remember to put your message in "quotation marks"!
 
 
 
 # Step 2:
-# Use the input function to ask the player to type in at least 5 types of words or numbers.
-# Be descriptive about what type of word or number they need to type in.
-# Store each of these inputted values into different variables that you can use later in your program.
-# Remember to include spaces at the end of your inputs!
+# [] Use the input function to ask the player to type in at least 5 types of words or numbers. Be descriptive about
+# what type of word or number they need to type in and remember to include spaces at the end of your inputs!
+# [] Store each of these inputted values into different variables that you can use later in your program.
 
 
 
@@ -17,6 +16,6 @@ print("")
 #################################
 
 # Step 3:
-# Use print statements and string concatenation to print your Mad Libs story!
+# [] Use print statements and string concatenation to print your Mad Libs story!
 # Remember to include spaces between the words that you are joining together!
 
