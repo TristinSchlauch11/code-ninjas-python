456fe710fb783cbc4cab85bf425f6ea2aae574e6
Tristin Schlauch <nmbr1hockeyfan@gmail.com>
2026-01-29 12:31:57 -0700

    added gravity rings

--- NEW Course - Pygame Zero Games/Block Bounce/block_bounce.py
+++ NEW Course - Pygame Zero Games/Block Bounce/block_bounce.py
@@ -6,37 +6,67 @@ HEIGHT = 500
 on_ground = True
 clicked = False
 x_offset = 0
+form = "block"
 
 # physics variables
 gravity = 1
 velocity = 0
 
 # sprites
-player = Actor("cone_straight")
+bg = Actor("background")
+bg.bottomleft = (0, 500)
+
+player = Actor("player")
 player.bottomleft = (200, 350)
 
-spikes = []
-squares = []
+# spike and platform sprites
+def create_obst(pos_list, image, can_land):
+    for (col, row) in pos_list:
+        # create the sprite and set its position
+        sprite = Actor(image)
+        sprite.left = 700 + 45*col
+        sprite.bottom = 350 - 45*row
+        # add it to the neccesary lists
+        all_obst.append(sprite)
+        if can_land:
+            platforms.append(sprite)
+
+platforms = []
 all_obst = []
 
-for i in range(1100, 1250, 50):
-    spike = Actor("cone_down")
-    spike.bottomleft = (i, 350)
-    spikes.append(spike)
-    all_obst.append(spike)
+block_coords = [(1,0), (7,2), (16, 4), (17, 4), (18, 4), (19, 4), (20, 4)]
+create_obst(block_coords, "block", True)
 
-for i in range(700, 900, 50):
-    square = Actor("cone_straight")
-    square.bottomleft = (i, 350)
-    squares.append(square)
-    all_obst.append(square)
+spike_coords = [(2,0), (3,0), (10,0), (11,0)]
+create_obst(spike_coords, "spike", False)
+
+# ring sprites
+def create_ring(pos_list, ring):
+    for (col, row) in pos_list:
+        # create the sprite and set its position
+        sprite = Actor(ring)
+        sprite.left = 700 + 45*col
+        sprite.bottom = 350 - 45*row
+        # add it to the neccesary lists
+        rings.append(sprite)
+
+rings = []
+
+up_coord = [(15, 0)]
+create_ring(up_coord, "up_ring")
+
+down_coord = [(21, 2)]
+create_ring(down_coord, "down_ring")
 
 def draw():
-    screen.fill((20, 50, 255))
+    bg.draw()
     player.draw()
 
     for obst in all_obst:
         obst.draw()
+
+    for ring in rings:
+        ring.draw()
 
 def update():
     global velocity
@@ -44,12 +74,19 @@ def update():
     global on_ground
 
     # player movement
-    if (keyboard.space or clicked) and on_ground:
-        velocity = -15
-    elif velocity < 25:
-        velocity += gravity
+    if form == "block":
+        if (keyboard.space or clicked) and on_ground:
+            velocity = -15*gravity
+        elif velocity < 25 and velocity > -25:
+            velocity += gravity
+        on_ground = False       # to prevent mid-air jumps
+    #testing!!
+    elif form == "rocket":
+        if keyboard.space or clicked:
+            velocity += -1*gravity
+        else:
+            velocity += gravity
 
-    on_ground = False
     player.y += velocity
 
     # check if player is on the floor
@@ -58,30 +95,45 @@ def update():
         velocity = 0
         on_ground = True
 
-    # check for collisions
+    # check for collision with obstacles
     o_index = player.collidelist(all_obst)
     if o_index != -1:
         # check what we hit
         obstacle = all_obst[o_index]
-        if obstacle in squares:
+        if obstacle in platforms:
             # check if we have landed on a square
-            if player.bottom < square.top + 25:
-                player.bottom = square.top
-                velocity = 0
-                on_ground = True
-            # we have hit a square
+            if gravity == 1:
+                if player.bottom < obstacle.top + 25:
+                    player.bottom = obstacle.top
+                    velocity = 0
+                    on_ground = True
+                # we have hit a square
+                else:
+                    reset_all_obst()
             else:
-                reset_all_obst()
+                if player.top > obstacle.bottom - 25:
+                    player.top = obstacle.bottom
+                    velocity = 0
+                    on_ground = True
+                # we have hit a square
+                else:
+                    reset_all_obst()
         # we have hit something else
         else:
             reset_all_obst()
+
+    # check for collision with rings
+    ring_index = player.collidelist(rings)
+    if ring_index != -1:
+        ring = rings[ring_index]
+        pass_ring(ring)
 
     # move all other stuff
     x_offset += 10
     for obst in all_obst:
         obst.x -= 10
-        if obst.right < -500:
-            reset_all_obst()
+    for ring in rings:
+        ring.x -= 10
 
 def on_mouse_down(button):
     global clicked
@@ -93,12 +145,41 @@ def on_mouse_up(button):
     if button == mouse.LEFT:
         clicked = False
 
+def on_key_down(key):
+    if key == keys.RETURN:
+        reset_all_obst()
+
+def pass_ring(ring):
+    global gravity
+    global form
+
+    if ring.image == "down_ring":
+        gravity = 1
+    elif ring.image == "up_ring":
+        gravity = -1
+    if ring.image == "block_ring":
+        player.image = "player"
+        form = "block"
+    elif ring.image == "rocket_ring":
+        form = "rocket"
+
 def reset_all_obst():
     global x_offset
+    global form
+    global gravity
+    global velocity
 
     # reset all obstacles
     for obst in all_obst:
         obst.x += x_offset
+    for ring in rings:
+        ring.x += x_offset
+
+    # reset player
+    player.form = "block"
+    player.bottomleft = (200, 350)
+    gravity = 1
+    velocity = 0
 
     # reset offset
     x_offset = 0
--- NEW Course - Pygame Zero Games/Space Shooters/python_class.py
+++ NEW Course - Pygame Zero Games/Space Shooters/python_class.py
@@ -1,4 +1,5 @@
 # Happy coding!
+import random
 
 WIDTH = 500
 HEIGHT = 700
@@ -7,18 +8,37 @@ spaceship = Actor("playership1_orange")
 spaceship.center = (WIDTH / 2, HEIGHT - 100)
 
 my_bullets = []
+enemy_bullets = []
+enemies = []
 
 bullet_cooldown = 0
+enemy_cooldown = 0
+
+game_over = False
+score = 0
 
 def draw():
-    screen.fill((100, 50, 255))
-    spaceship.draw()
+    if game_over:
+        screen.fill((0,0,0))
+    else:
+        screen.fill((100, 50, 255))
+        screen.draw.text("Score: " + str(score), midright = (WIDTH - 25, HEIGHT - 25))
+        spaceship.draw()
 
-    for bullet in my_bullets:
-        bullet.draw()
+        for enemy in enemies:
+            enemy.draw()
+
+        for bullet in my_bullets:
+            bullet.draw()
+
+        for bullet in enemy_bullets:
+            bullet.draw()
 
 def update():
     global bullet_cooldown
+    global enemy_cooldown
+    global game_over
+    global score
 
     if keyboard.a and spaceship.left > 25:
         spaceship.x -= 5
@@ -36,3 +56,37 @@ def update():
         bullet.y -= 25
         if bullet.y < -50:
             my_bullets.remove(bullet)
+
+    for enemy in enemies:
+        enemy.y += 3
+
+    for bullet in enemy_bullets:
+        bullet.y += 10
+        if bullet.y > HEIGHT + 50:
+            enemy_bullets.remove(bullet)
+
+    for enemy in enemies:
+        if enemy_cooldown > 0:
+            enemy_cooldown -= 1
+        else:
+            bullet = Actor("laserblue16")
+            bullet.midtop = enemy.midtop
+            enemy_bullets.append(bullet)
+            enemy_cooldown = 60
+
+    for enemy in enemies:
+        bullet_index = enemy.collidelist(my_bullets)
+        if bullet_index != -1:
+            enemies.remove(enemy)
+            del my_bullets[bullet_index]
+            score += 1
+
+    if spaceship.collidelist(enemy_bullets) != -1:
+        game_over = True
+
+def create_enemy():
+    enemy = Actor("enemyblue2")
+    enemy.center = (73 + 118*random.randint(0, 3), 100)
+    enemies.append(enemy)
+
+clock.schedule_interval(create_enemy, 1.5)
